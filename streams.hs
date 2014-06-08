ca67089a5be00223120773256d4cc6ac6d1cb2c2
Peter Salvi <vukung@yahoo.com>
2014-06-08 19:25:26 +0200

    Final assignment (Scala & Haskell).

---
+++ streams.hs
@@ -0,0 +1,124 @@
+import Control.Arrow ((&&&))
+import Control.Monad.Reader
+import Data.Array
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Prelude hiding (Left, Right)
+  
+data Pos     = Pos   Int Int deriving (Eq, Ord, Show)
+data Block   = Block Pos Pos deriving (Eq, Ord, Show)
+type Terrain = Pos -> Bool
+data Move    = Left | Right | Up | Down deriving (Show)
+data Level   = Level { terrain :: Terrain, startPos :: Pos, goal :: Pos }
+
+posDx (Pos x y) d = Pos (x + d) y
+posDy (Pos x y) d = Pos x (y + d)
+
+blockDx (Block p q) (d1, d2) = Block (posDx p d1) (posDx q d2)
+blockDy (Block p q) (d1, d2) = Block (posDy p d1) (posDy q d2)
+
+-- 1a
+terrainFunction :: Array Int (Array Int Char) -> Pos -> Bool
+terrainFunction t (Pos x _) | not $ inRange (bounds t) x       = False
+terrainFunction t (Pos _ y) | not $ inRange (bounds (t ! 0)) y = False
+terrainFunction t (Pos x y)                                    = (t ! x) ! y /= '-'
+-- 1b
+findChar :: Char -> Array Int (Array Int Char) -> Pos
+findChar c t = let x = fst $ head $ filter (elem c . elems . snd) $ assocs t
+                   y = fst $ head $ filter ((== c) . snd) $ assocs (t ! x)
+               in Pos x y
+
+-- 2a
+isStanding :: Block -> Bool
+isStanding (Block p q) = p == q
+-- 2b
+isLegal :: Block -> Reader Level Bool
+isLegal (Block p q) = do t <- asks terrain
+                         return $ t p && t q
+-- 2c
+startBlock :: Reader Level Block
+startBlock = do sp <- asks startPos
+                return $ Block sp sp
+
+left  b                               | isStanding b = blockDy b (-2, -1)
+left  b@(Block (Pos px _) (Pos qx _)) | px == qx     = blockDy b (-1, -2)
+left  b                                              = blockDy b (-1, -1)
+right b                               | isStanding b = blockDy b ( 1,  2)
+right b@(Block (Pos px _) (Pos qx _)) | px == qx     = blockDy b ( 2,  1)
+right b                                              = blockDy b ( 1,  1)
+up    b                               | isStanding b = blockDx b (-2, -1)
+up    b@(Block (Pos px _) (Pos qx _)) | px == qx     = blockDx b (-1, -1)
+up    b                                              = blockDx b (-1, -2)
+down  b                               | isStanding b = blockDx b ( 1,  2)
+down  b@(Block (Pos px _) (Pos qx _)) | px == qx     = blockDx b ( 1,  1)
+down  b                                              = blockDx b ( 2,  1)
+
+-- 3a
+neighbors :: Block -> [(Block, Move)]
+neighbors b = [(left b, Left), (right b, Right), (up b, Up), (down b, Down)]
+-- 3b
+legalNeighbors :: Block -> Reader Level [(Block, Move)]
+legalNeighbors = filterM (isLegal . fst) . neighbors
+
+-- 4a
+done :: Block -> Reader Level Bool
+done b@(Block p _) = do g <- asks goal
+                        return $ isStanding b && p == g
+-- 4b
+neighborsWithHistory :: Block -> [Move] -> Reader Level [(Block, [Move])]
+neighborsWithHistory b h = do n <- legalNeighbors b
+                              return $ map (fst &&& (: h) . snd) n
+-- 4c
+newNeighborsOnly :: Set Block -> [(Block, [Move])] -> [(Block, [Move])]
+newNeighborsOnly e = filter (flip Set.notMember e . fst)
+-- 4d
+from :: [(Block, [Move])] -> Set Block -> Reader Level [(Block, [Move])]
+from [] _ = return []
+from i  e = do n <- mapM (uncurry neighborsWithHistory) i
+               let next_i = concat $ map (newNeighborsOnly e) n
+               let next_e = Set.union (Set.fromList $ map fst i) e
+               fmap (i ++) $ from next_i next_e
+-- 4e/1
+pathsFromStart :: Reader Level [(Block, [Move])]
+pathsFromStart = do start <- startBlock
+                    from [(start, [])] Set.empty
+-- 4e/2
+pathsToGoal :: Reader Level [(Block, [Move])]
+pathsToGoal = do paths <- pathsFromStart
+                 filterM (done . fst) paths
+-- 4e/3
+solution :: Reader Level [Move]
+solution = do paths <- pathsToGoal
+              return $ if null paths then [] else snd (head paths)
+
+-- Test
+
+readLevel :: [String] -> Level
+readLevel xs = Level (terrainFunction a) (findChar 'S' a) (findChar 'T' a)
+  where n = length xs
+        m = length (head xs)
+        a = listArray (0, n-1) $ map (listArray (0, m-1)) xs
+
+level1 = ["ooo-------",
+          "oSoooo----",
+          "ooooooooo-",
+          "-ooooooooo",
+          "-----ooToo",
+          "------ooo-"]
+
+main = putStrLn $ show $ runReader solution (readLevel level1)
+
+-- Check
+
+step :: Move -> Block -> Block
+step Left  = left
+step Right = right
+step Up    = up
+step Down  = down
+
+checkSolution :: Level -> [Move] -> Bool
+checkSolution l xs = foldl (flip step) start xs == Block end end
+  where start = runReader startBlock l
+        end   = goal l
+
+-- prop> checkSolution level $ runReader solution level
---
+++ streams.scala
@@ -0,0 +1,111 @@
+package streams
+import common._
+
+trait GameDef {
+  case class Pos(x: Int, y: Int) {
+    def dx(d: Int) = copy(x = x + d)
+    def dy(d: Int) = copy(y = y + d)
+  }
+  val startPos: Pos
+  val goal: Pos
+
+  type Terrain = Pos => Boolean
+  val terrain: Terrain
+
+  sealed abstract class Move
+  case object Left  extends Move
+  case object Right extends Move
+  case object Up    extends Move
+  case object Down  extends Move
+
+  case class Block(b1: Pos, b2: Pos) {
+    require(b1.x <= b2.x && b1.y <= b2.y, "Invalid block position: b1=" + b1 + ", b2=" + b2)
+    def dx(d1: Int, d2: Int) = Block(b1.dx(d1), b2.dx(d2))
+    def dy(d1: Int, d2: Int) = Block(b1.dy(d1), b2.dy(d2))
+    def left = if (isStanding)         dy(-2, -1)
+               else if (b1.x == b2.x)  dy(-1, -2)
+               else                    dy(-1, -1)
+    def right = if (isStanding)        dy(1, 2)
+                else if (b1.x == b2.x) dy(2, 1)
+                else                   dy(1, 1)
+    def up = if (isStanding)           dx(-2, -1)
+             else if (b1.x == b2.x)    dx(-1, -1)
+             else                      dx(-1, -2)
+    def down = if (isStanding)         dx(1, 2)
+               else if (b1.x == b2.x)  dx(1, 1)
+               else                    dx(2, 1)
+    // ex. 3a
+    def neighbors: List[(Block, Move)] = List((left, Left), (right, Right), (up, Up), (down, Down))
+    // ex. 3b
+    def legalNeighbors: List[(Block, Move)] = neighbors.filter(_._1.isLegal)
+    // ex. 2a
+    def isStanding: Boolean = b1 == b2
+    // ex. 2b
+    def isLegal: Boolean = terrain(b1) && terrain(b2)
+  }
+  // ex. 2c
+  def startBlock: Block = Block(startPos, startPos)
+}
+
+trait StringParserTerrain extends GameDef {
+  val level: String
+  // ex. 1a
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = (p: Pos) => {
+    if (p.x < 0 || p.y < 0 || p.x >= levelVector.size || p.y >= levelVector(0).size)
+      false
+    else
+      levelVector(p.x)(p.y) != '-'
+  }
+  // ex. 1b
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    val x = levelVector.indexWhere((v: Vector[Char]) => v.contains(c))
+    val y = levelVector(x).indexOf(c) // searches the row x twice...
+    Pos(x,y)
+  }
+
+  private lazy val vector: Vector[Vector[Char]] =
+    Vector(level.split("\n").map(str => Vector(str: _*)): _*)
+
+  lazy val terrain: Terrain = terrainFunction(vector)
+  lazy val startPos: Pos = findChar('S', vector)
+  lazy val goal: Pos = findChar('T', vector)
+}
+
+trait Solver extends GameDef {
+  // ex. 4a
+  def done(b: Block): Boolean = b.isStanding && b.b1 == goal
+
+  // ex. 4b
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = {
+    // thi implementation defeats the purpose of lazy streams...
+    // but it should be OK, as legalNeighbors has at most 4 items
+    (for (neighbor <- b.legalNeighbors)
+     yield (neighbor._1, neighbor._2 :: history)).toStream
+  }
+
+  // ex. 4c
+  def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
+                       explored: Set[Block]): Stream[(Block, List[Move])] = {
+    neighbors.filter((x: (Block, List[Move])) => !explored.contains(x._1))
+  }
+
+  // ex. 4d
+  def from(initial: Stream[(Block, List[Move])],
+           explored: Set[Block]): Stream[(Block, List[Move])] = {
+    if (initial.isEmpty)
+      Stream()
+    else {
+      val next =
+        initial.flatMap((x) => newNeighborsOnly(neighborsWithHistory(x._1, x._2), explored))
+      initial #::: from(next, explored ++ initial.map(_._1))
+    }
+  }
+
+  // ex. 4e/1
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(Stream((startBlock, List())), Set())
+  // ex. 4e/2
+  lazy val pathsToGoal: Stream[(Block, List[Move])] =
+    pathsFromStart.filter((x: (Block, List[Move])) => done(x._1))
+  // ex. 4e/3
+  lazy val solution: List[Move] = if (pathsToGoal.isEmpty) Nil else pathsToGoal.head._2
+}
